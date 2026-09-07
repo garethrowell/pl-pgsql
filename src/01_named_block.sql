6a8e61f609d4eaa0b7398ba15464bbfaf3525012
Gareth A Rowell <gar417@gmail.com>
2026-09-07 11:58:48 -0500

    Add files via upload

---
+++ src/01_named_block.sql
@@ -0,0 +1,15 @@
+
+create or replace function factorial(num integer)
+returns integer as $$
+declare
+  result integer := 1;
+begin
+	<<factorial_loop>>
+	for i in 1..num loop
+		result := result * i;
+	end loop factorial_loop;
+	return result;
+end;
+$$ language plpgsql;
+
+
